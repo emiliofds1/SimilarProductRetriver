5f635303f6a2c4fbc9cd3c7aab9ba78fb2caebdb
Emilio Fernandez <emiliofds@gmail.com>
2022-10-05 17:32:55 +0200

    init project

---
+++ SimilarProductRetriver/src/main/java/com/devtest/SimilarProductRetriver/SimilarProductRetriverApplication.java
@@ -0,0 +1,13 @@
+package com.devtest.SimilarProductRetriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductRetriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductRetriverApplication.class, args);
+	}
+
+}
---
+++ SimilarProductRetriver/src/test/java/com/devtest/SimilarProductRetriver/SimilarProductRetriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtest.SimilarProductRetriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductRetriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
